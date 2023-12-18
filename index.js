0bfd5fb0202c7195c1af992e8e264ff4ebda00e3
UIT-20521229 <75383352+UIT-20521229@users.noreply.github.com>
2023-12-19 00:25:46 +0700

    upload file into main branch

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = process.env.DATABASE_PORT;
+const dbURL = process.env.DATABASE_URL;
+
+const app = express();
+const routes = require('./routes/routes');
+
+app.use(express.json());
+app.use('/api', routes)
+
+const bodyParser = require('body-parser');
+const mongoString = dbURL;
+mongoose.connect(mongoString)
+
+const database = mongoose.connection;
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
+})
